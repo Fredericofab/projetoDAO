15aabc2ad1de27d5f91a148a23fb9101fad5c520
Frederico A Barros <frederico.fab@gmail.com>
2020-01-03 11:02:16 -0300

    236 Classe Vendedor

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,6 +1,9 @@
 package aplicacao;
 
+import java.util.Date;
+
 import modelo.entidades.Departamento;
+import modelo.entidades.Vendedor;
 
 public class Programa {
 
@@ -9,6 +12,8 @@ public class Programa {
 		Departamento depto = new Departamento(1, "livros");
 		System.out.println(depto);
 
+		Vendedor vendedor = new Vendedor(21, "fred", "fred@gmail.com", new Date(), 3000.01, depto);
+		System.out.println(vendedor);
 	}
 
 }
